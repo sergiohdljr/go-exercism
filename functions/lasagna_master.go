580eda598910b5aa8231e58b2a7a8c84b7c4c5fd
sergiohdljr <sergio.tjf1@gmail.com>
2024-04-10 19:04:45 -0300

    functions: define the others functions

--- functions/lasagna_master.go
+++ functions/lasagna_master.go
@@ -1,9 +1,25 @@
 package lasagna
 
+// TODO: define the 'PreparationTime()' function
 func PreparationTime(layers []string, prepPerLayer int) int {
 	if prepPerLayer == 0 {
 		prepPerLayer = 2
 	}
 	numberOfLayers := len(layers)
 	return numberOfLayers * prepPerLayer
+}
+
+// TODO: define the 'Quantities()' function
+func Quantities(slice []string) (int, float64) {
+	panic("error")
+}
+
+// TODO: define the 'AddSecretIngredient()' function
+func AddSecretIngredient(friendList []string, myList []string) {
+	panic("error")
+}
+
+// TODO: define the 'ScaleRecipe()' function
+func ScaleRecipe(quantities []float64, portions int) []float64 {
+	panic("error")
 }
